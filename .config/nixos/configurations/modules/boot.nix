d640bf574be5997eae8d6284a9253307f9478221
xitofa <ibn.al.khaafi@gmail.com>
2025-11-03 17:09:01 +0100

    Add boot configuration for NixOS

---
+++ .config/nixos/configurations/modules/boot.nix
@@ -0,0 +1,20 @@
+# /etc/nixos/configurations/modules/boot.nix
+{ config, pkgs, lib, ... }:
+
+{
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernel.sysctl = {
+      "vm.swappiness" = 10;
+      "vm.vfs_cache_pressure" = 50;
+      "net.ipv4.tcp_congestion_control" = "bbr";
+    };
+
+    kernelParams = [ "cpufreq.default_govenor=schedutil" ];
+  };
+}
