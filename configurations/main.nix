1aa87459a4ee01b9cd4c958a5996886227483b6a
xitofa <ibn.al.khaafi@gmail.com>
2025-11-03 17:30:22 +0100

    Add main NixOS configuration file

--- configurations/main.nix
+++ configurations/main.nix
@@ -1,1 +1,17 @@
+# /etc/nixos/configurations/main.nix
+{ config, pkgs, lib, ... }:
 
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ./modules/boot.nix
+    ./modules/desktop.nix
+    ./modules/hardware.nix
+    ./modules/locale.nix
+    ./modules/settings.nix
+    ./modules/packages.nix
+  ];
+
+  system.stateVersion = "25.05";
+}
