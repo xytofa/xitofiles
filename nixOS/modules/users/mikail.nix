99d37f306c37d704fb28b175e625215f47d5b526
xitofa <ibn.al.khaafi@gmail.com>
2025-11-12 18:05:42 +0100

    Add user configuration for Mikail with packages
    This file defines user 'mikail' with specific packages and groups.

---
+++ nixOS/modules/users/mikail.nix
@@ -0,0 +1,35 @@
+# /etc/nixos/modules/users/mikail.nix
+
+{ pkgs, ... }:
+
+{
+  users.users.mikail = {
+    isNormalUser = true;
+    description = "Mikail";
+    extraGroups = [ "networkmanager" "wheel" ];
+
+    packages = with pkgs; [
+      # Editors/CLI
+      micro
+      alacritty
+      wl-clipboard
+      fastfetch
+
+      # Browsers/Media
+      firefox
+      vlc
+
+      # Gaming
+      steam
+      vesktop
+      protontricks
+      mangohud
+      gamemode
+      prismlauncher
+
+      # Fonts
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.zed-mono
+    ];
+  };
+}
