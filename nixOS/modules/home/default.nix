6bc448ae58134cccae961b1a6fca1ecc6df36ff4
xitofa <ibn.al.khaafi@gmail.com>
2025-11-18 19:00:09 +0100

    Update default.nix

--- nixOS/modules/home/default.nix
+++ nixOS/modules/home/default.nix
@@ -1,35 +1,10 @@
-# /etc/nixos/modules/home/default.nix
-
+# /etc/nixos/modules/users/default.nix
 { pkgs, ... }:
 
 {
   users.users.mikail = {
     isNormalUser = true;
-    description = "Mikail";
+    description = "mikail";
     extraGroups = [ "networkmanager" "wheel" ];
-
-    packages = with pkgs; [
-      # Editors/CLI
-      micro
-      alacritty
-      wl-clipboard
-      fastfetch
-
-      # Browsers/Media
-      firefox
-      vlc
-
-      # Gaming
-      steam
-      vesktop
-      protontricks
-      mangohud
-      gamemode
-      prismlauncher
-
-      # Fonts
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.zed-mono
-    ];
   };
 }
