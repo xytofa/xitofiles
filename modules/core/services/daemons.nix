fb8b33520df230283f53730d877db1b7c433fb70
xitofa <ibn.al.khaafi@gmail.com>
2025-11-21 19:03:00 +0100

    Create daemons.nix

---
+++ modules/core/services/daemons.nix
@@ -0,0 +1,35 @@
+# /etc/nixos/modules/core/services/daemons.nix
+{ config, lib, pkgs, ... }:
+
+{
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
+    };
+
+    flatpak.enable = true;
+    dbus.enable = true;
+
+    xserver = {
+      enable = true;
+      videoDrivers = [ "nvidia" ];
+    };
+
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    firewall.enable = true;
+  };
+
+  security = {
+    polkit.enable = true;
+    rtkit.enable = true;
+  };
+}
